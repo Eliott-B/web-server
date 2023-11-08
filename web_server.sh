019080ccd96f60d44119e51788df4602f75fc3ee
Eliott-B <eliottb.info@gmail.com>
2023-11-08 19:05:08 +0100

    :bug: | Fix Tips

--- web_server.sh
+++ web_server.sh
@@ -2,9 +2,9 @@
 #AUTHOR: ELIOTT-B
 
 #COLOR
-RED='\033[0;31m'
-YELLOW='\033[0;33m'
-NC='\033[0m'
+NC="$(tput sgr0)"
+RED="$(tput bold; tput setaf 1)";
+YELLOW="$(tput bold; tput setaf 3)";
 
 which php > /dev/null
 if [ $? -eq 1 ]
@@ -12,9 +12,9 @@ then
     echo "${RED}[ERROR] PHP is not installed on your machine!${NC}"
     exit 1
 fi
+echo "${YELLOW}[TIPS] To shutdown the server, press 'CTRL + C' (LINUX) or 'CONTROL + C' (MACOS).${NC}"
 if [ $# -eq 0 ]
 then
-    echo "${YELLOW}[TIPS] To shutdown the server, press 'CTRL + C' (LINUX) or 'CONTROL + C' (MACOS).${NC}"
     php -S localhost:8080 -t .
 else
     php -S localhost:8080 -t $1
